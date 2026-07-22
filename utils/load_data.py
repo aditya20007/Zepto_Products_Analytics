140a5c5d80ff3e1d94d9046179801e1eda4914d3
Aditya Singh <singhadityakumar566@gmail.com>
2026-07-22 16:19:13 +0530

    Update Zepto Product Analytics Dashboard

--- utils/load_data.py
+++ utils/load_data.py
@@ -12,6 +12,13 @@ from sqlalchemy import text
 from utils.database import get_engine
 from utils.queries import QUERIES
 
+# Debug Environment Variables
+print("DB_HOST =", os.getenv("DB_HOST"))
+print("DB_PORT =", os.getenv("DB_PORT"))
+print("DB_NAME =", os.getenv("DB_NAME"))
+print("DB_USER =", os.getenv("DB_USER"))
+print("DB_PASSWORD =", os.getenv("DB_PASSWORD"))
+
 
 # =====================================================
 # Generic SQL Executor
